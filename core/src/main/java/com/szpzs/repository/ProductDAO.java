35a3918bcdfbc6c861c7521f4cdd4a840f3fc7d6
peterszopka <szopka.peter@openminds.hu>
2014-06-03 14:15:16 +0200

    #2 write the main methods

--- core/src/main/java/com/szpzs/repository/ProductDAO.java
+++ core/src/main/java/com/szpzs/repository/ProductDAO.java
@@ -1,9 +1,17 @@
 package com.szpzs.repository;
 
+import java.math.BigInteger;
+import java.util.List;
+
 import com.szpzs.model.Product;
 
 public interface ProductDAO {
-	public Product getProductById(int id);
 	
-	public void addProduct(Product product);
+	public List<Product> getProductList();
+	
+	public void saveProduct(Product product);
+	
+	public Product getProduct(Long id);
+	
+	public void update (Product product);
 }
--- core/src/main/java/com/szpzs/repository/ProductDAOImpl.java
+++ core/src/main/java/com/szpzs/repository/ProductDAOImpl.java
@@ -1,32 +1,49 @@
 package com.szpzs.repository;
 
+import java.math.BigInteger;
+import java.util.List;
+
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.szpzs.model.Product;
 
 @Repository
-public class ProductDAOImpl implements ProductDAO {
+public class ProductDAOImpl implements ProductDAO{
 	
-	@Autowired private SessionFactory sessionFactory;
+	@Autowired 
+	private SessionFactory sessionFactory;
 	
 	@Override
-	public Product getProductById(int id) {
+	public List<Product> getProductList() {
 		Session session = sessionFactory.getCurrentSession();
-		Query  query = session.createQuery("Select p from Product as p where p.id = :id");
-		query.setParameter("id", id);
-		Product product = (Product) query.uniqueResult();
+		List products = session.createQuery("Select p from Product as p").list();
+		return products;
+	}
+
+	@Override
+	public void saveProduct(Product product) {
+		Session session = sessionFactory.getCurrentSession();
+		session.save(product);
+	}
+	
+	@Override
+	public Product getProduct(Long id) {
+		Session session = sessionFactory.getCurrentSession();
+		Product product = (Product) session.get(Product.class, id);
 		return product;
 	}
 
 	@Override
-	public void addProduct(Product product) {
+	public void update(Product product) {
 		Session session = sessionFactory.getCurrentSession();
-		session.save(product);
+		session.merge(product);
 	}
 
-	
+
+
 }
