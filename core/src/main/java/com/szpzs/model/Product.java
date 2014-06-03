91645f7f7e2bef3db813056fe1f6bcfcf2440ea0
peterszopka <szopka.peter@openminds.hu>
2014-06-03 14:12:18 +0200

    #2 add sequence settings

---
+++ core/src/main/java/com/szpzs/model/Product.java
@@ -0,0 +1,108 @@
+package com.szpzs.model;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import java.math.BigInteger;
+
+
+/**
+ * The persistent class for the PRODUCTS database table.
+ * 
+ */
+@Entity
+@Table(name="PRODUCTS")
+@NamedQuery(name="Product.findAll", query="SELECT p FROM Product p")
+public class Product implements Serializable {
+	private static final Long serialVersionUID = 1L;
+	
+	@SequenceGenerator(name="Prod", sequenceName="PROD_SEQ")
+	@Id @GeneratedValue(generator="Prod")
+	private Long id;
+
+	private BigInteger area;
+
+	@Column(name="CATEGORY_ID")
+	private BigInteger categoryId;
+
+	@Column(name="CITY_ID")
+	private BigInteger cityId;
+
+	private String descriptions;
+
+	private String name;
+
+	@Column(name="OWNER_ID")
+	private BigInteger ownerId;
+
+	private BigInteger status;
+
+	public Product() {
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigInteger getArea() {
+		return this.area;
+	}
+
+	public void setArea(BigInteger area) {
+		this.area = area;
+	}
+
+	public BigInteger getCategoryId() {
+		return this.categoryId;
+	}
+
+	public void setCategoryId(BigInteger categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public BigInteger getCityId() {
+		return this.cityId;
+	}
+
+	public void setCityId(BigInteger cityId) {
+		this.cityId = cityId;
+	}
+
+	public String getDescriptions() {
+		return this.descriptions;
+	}
+
+	public void setDescriptions(String descriptions) {
+		this.descriptions = descriptions;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigInteger getOwnerId() {
+		return this.ownerId;
+	}
+
+	public void setOwnerId(BigInteger ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public BigInteger getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(BigInteger status) {
+		this.status = status;
+	}
+
+}
