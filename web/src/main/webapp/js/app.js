4aaaca561ae253c5fc7e17c7ac6086cbf792463b
Zseee <osvath.zsolt@openminds.hu>
2014-07-15 11:37:12 +0200

    #20 Modified routeProvider

--- web/src/main/webapp/js/app.js
+++ web/src/main/webapp/js/app.js
@@ -2,11 +2,34 @@ var goods_exchange = angular.module('goods_exchange', [
     'ngRoute',
     'LocalStorageModule',
     'registrationCtrl',
-    'productUploadCtrl',
+    'productCtrl',
     'loginCtrl',
     'formDirectives',
     'services'
 ]);
+
+window.routes =
+{
+    "/index": {
+    	templateUrl: 'pages/index.html',
+        requireLogin: false
+    },
+    "/productUpload": {
+    	templateUrl: 'pages/product_upload.html', 
+        controller: 'productUploadCtrl', 
+        requireLogin: true,
+    },
+    "/productUpdate": {
+    	templateUrl: 'pages/product_upload.html', 
+        controller: 'productUpdateCtrl', 
+        requireLogin: true,
+        requireProduct: true,
+    },
+    "/product":{
+	    templateUrl: 'pages/product.html', 
+	    requireLogin: false,
+    }
+};
 
 
 goods_exchange.config(['$routeProvider', '$locationProvider', '$httpProvider','localStorageServiceProvider',
@@ -14,30 +37,35 @@ goods_exchange.config(['$routeProvider', '$locationProvider', '$httpProvider','l
 		localStorageServiceProvider.setStorageType('sessionStorage');
 		$httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    $routeProvider.
-        when('/index', {
-            templateUrl: 'pages/index.html',
-        }).
-        when('/productUpload', {
-            templateUrl: 'pages/product_upload.html',
-        }).
-        when('/productUpdate', {
-            templateUrl: 'pages/product_upload.html',
-        }).
-        when('/product', {
-            templateUrl: 'pages/product.html',
-        }).
-        otherwise({
-            redirectTo: '/index'
-        }); 
+    
+    for(var path in window.routes) {
+        $routeProvider.when(path, window.routes[path]);
+    }
+    $routeProvider.otherwise({
+    	redirectTo: '/index'
+    }); 
   }]);
 
-goods_exchange.run(function ($rootScope, localStorageService, cityServices, categoryServices) {
+goods_exchange.run(function ($rootScope, localStorageService, cityServices, categoryServices, $location) {
     $rootScope.header = 'pages/header.html';
     $rootScope.regModal = 'pages/regModal.html';
     $rootScope.footer = 'pages/footer.html';
-    $rootScope.updateProduct = false;
     $rootScope.currentProduct = null;
+    
+    
+    $rootScope.$on("$locationChangeStart", function(event, next, current) {
+        for(var i in window.routes) {
+            if(next.indexOf(i) != -1) {
+                if(window.routes[i].requireLogin && !$rootScope.loggedUser) {
+                	$location.path('/index');
+                	$("#loginModal").modal("show");
+                }
+                if(window.routes[i].requireProduct && !$rootScope.currentProduct){
+                	$location.path('/index');
+                }
+            }
+        }
+    });
     
     cityServices.getCities().success(function(cities){
     	$rootScope.cities = cities;
