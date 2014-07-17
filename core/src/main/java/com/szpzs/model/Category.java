58f5cc0c3309e20a9f4fba2695db66dc063b868b
Klaunyinya <kecskes.klaudia@openminds.hu>
2014-07-17 15:37:59 +0200

    #28 Rename class from Categories to Category

---
+++ core/src/main/java/com/szpzs/model/Category.java
@@ -0,0 +1,65 @@
+package com.szpzs.model;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import java.math.BigInteger;
+import java.util.List;
+
+
+/**
+ * The persistent class for the CATEGORIES database table.
+ * 
+ */
+@Entity
+@Table(name="CATEGORIES")
+
+@NamedQuery(name="Category.findAll", query="SELECT c FROM Category c")
+public class Category implements Serializable {
+
+	private static final Long serialVersionUID = 1L;
+	
+	@SequenceGenerator(name="Cat", sequenceName="CATEGORIES_SEQ")
+	@Id @GeneratedValue(generator="Cat")
+	private Long id;
+
+	private String name;
+
+	@Column(name="PARENT_ID")
+	private BigInteger parentId;
+	
+	public Category() {
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigInteger getParentId() {
+		return this.parentId;
+	}
+
+	public void setParentId(BigInteger parentId) {
+		this.parentId = parentId;
+	}
+
+	public Object getSubCategories() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
